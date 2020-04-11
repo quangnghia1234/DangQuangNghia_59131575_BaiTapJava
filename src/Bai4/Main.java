9cf961fa4f69df26f4d30c963891fc66703cfd88
Laptop <Laptop@LAPTOP-V604OQUI>
2020-04-11 18:40:58 +0700

    up bai 4

--- src/Bai4/Main.java
+++ src/Bai4/Main.java
@@ -13,11 +13,11 @@ public class Main {
     public static void main(String[] args) {
         QuanLyChuyenXe QLCX = new QuanLyChuyenXe();
         ChuyenXeNoiThanh CXNoT1 = new ChuyenXeNoiThanh(3, 100, "MS123", "Đặng Quang Nghĩa", 59131575, 2000000);
-        ChuyenXeNoiThanh CXNoT2 = new ChuyenXeNoiThanh(4, 130, "MS124", "Đặng Quang Qua", 59131575, 2400000);
+        ChuyenXeNoiThanh CXNoT2 = new ChuyenXeNoiThanh(4, 130, "MS124", "Nguyen thien thua", 59131575, 2400000);
         ChuyenXeNoiThanh CXNoT3 = new ChuyenXeNoiThanh(5, 150, "MS125", "Đặng tấn Tín", 59131575, 2500000);
         ChuyenXeNgoaiThanh CXNgT1 = new ChuyenXeNgoaiThanh("Nha Trang", 4, "MS126", "Nguyễn Tấn Thi", 483833, 1000000);
-        ChuyenXeNgoaiThanh CXNgT2 = new ChuyenXeNgoaiThanh("Huế", 4, "MS127", "Nguyễn Tấn Thi", 483833, 1000000);
-        ChuyenXeNgoaiThanh CXNgT3 = new ChuyenXeNgoaiThanh("Đà Nẵng", 4, "MS128", "Nguyễn Tấn Thi", 483833, 1000000);
+        ChuyenXeNgoaiThanh CXNgT2 = new ChuyenXeNgoaiThanh("Huế", 4, "MS127", "Le Xuan Trung", 483833, 1000000);
+        ChuyenXeNgoaiThanh CXNgT3 = new ChuyenXeNgoaiThanh("Đà Nẵng", 4, "MS128", "Nguyễn Tấn Kiet", 483833, 1000000);
         
         QLCX.themChuyenXe(CXNoT1);
         QLCX.themChuyenXe(CXNoT2);
