bdf6ad2d175918bc414ddbc146868963dc91b1dc
Laptop <Laptop@LAPTOP-V604OQUI>
2020-04-11 15:30:42 +0700

    up bai 4

---
+++ src/Bai4/Main.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Bai4;
+
+/**
+ *
+ * @author Laptop
+ */
+public class Main {
+    public static void main(String[] args) {
+        QuanLyChuyenXe QLCX = new QuanLyChuyenXe();
+        ChuyenXeNoiThanh CXNoT1 = new ChuyenXeNoiThanh(3, 100, "MS123", "Đặng Quang Nghĩa", 59131575, 2000000);
+        ChuyenXeNoiThanh CXNoT2 = new ChuyenXeNoiThanh(4, 130, "MS124", "Đặng Quang Qua", 59131575, 2400000);
+        ChuyenXeNoiThanh CXNoT3 = new ChuyenXeNoiThanh(5, 150, "MS125", "Đặng tấn Tín", 59131575, 2500000);
+        ChuyenXeNgoaiThanh CXNgT1 = new ChuyenXeNgoaiThanh("Nha Trang", 4, "MS126", "Nguyễn Tấn Thi", 483833, 1000000);
+        ChuyenXeNgoaiThanh CXNgT2 = new ChuyenXeNgoaiThanh("Huế", 4, "MS127", "Nguyễn Tấn Thi", 483833, 1000000);
+        ChuyenXeNgoaiThanh CXNgT3 = new ChuyenXeNgoaiThanh("Đà Nẵng", 4, "MS128", "Nguyễn Tấn Thi", 483833, 1000000);
+        
+        QLCX.themChuyenXe(CXNoT1);
+        QLCX.themChuyenXe(CXNoT2);
+        QLCX.themChuyenXe(CXNoT3);
+        QLCX.themChuyenXe(CXNgT1);
+        QLCX.themChuyenXe(CXNgT2);
+        QLCX.themChuyenXe(CXNgT3);
+        
+        QLCX.inDSCX();
+        
+        System.out.println("Tổng doanh thu chuyến xe ngoại thành :"+QLCX.tinhDoanhThuNgoaiThanh());
+        System.out.println("Tổng doanh thu chuyến xe nội thành :"+QLCX.tinhDoanhThuNoiThanh());
+        System.out.println("tổng doanh thu cả 2 loại xe :"+QLCX.tongDoanhThu());
+    }
+}
---
+++ src/Bai4/QuanLyChuyenXe.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Bai4;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Laptop
+ */
+public class QuanLyChuyenXe {
+    ArrayList<ChuyenXe> DSCX = new ArrayList<>();
+
+    public QuanLyChuyenXe() {
+    }
+    
+    public void themChuyenXe(ChuyenXe Cx){
+        DSCX.add(Cx);
+    }
+    public void inDSCX(){
+        for(int i=0;i<DSCX.size();i++)
+            DSCX.get(i).hienthiTTCX();
+    }
+    public double tinhDoanhThuNoiThanh()
+    {
+        double S = 0;
+        for(int i=0;i < DSCX.size(); i++)
+            if(DSCX.get(i) instanceof ChuyenXeNoiThanh){
+            S += DSCX.get(i).getDoanhThu();
+            }
+        return S;
+    }
+    public double tinhDoanhThuNgoaiThanh()
+    {
+        double S = 0;
+        for(int i=0 ; i< DSCX.size(); i++)
+            if(DSCX.get(i) instanceof ChuyenXeNgoaiThanh){
+            S += DSCX.get(i).getDoanhThu();
+            }
+        return S;
+    }
+    public double tongDoanhThu()
+    {
+        double S = 0;
+        for(int i=0 ; i< DSCX.size(); i++){
+            S += DSCX.get(i).getDoanhThu();
+        }
+        return S;
+    }
+}
+
